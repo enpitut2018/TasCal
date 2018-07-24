d3a988ec2b8825329db1d9a5eaf76c8b77159ccd
Hakumai <inside.rice164@gmail.com>
2018-07-24 15:53:53 +0900

    挿入したタスクを画面に表示する処理を追加（タスク名が表示できていない）

--- app/controllers/tasklist_controller.rb
+++ app/controllers/tasklist_controller.rb
@@ -1,9 +1,11 @@
 class TasklistController < ApplicationController  
   def insert
-    # if request.post? then      
-    #   name = params['taskname']
-    #   date = params['date']
-    # end
+    if request.post? then
+      name = params['taskname']
+      date = params['date']
+
+      Tasklist.addtasks(name, date)
+    end
   end
 
   def create
@@ -15,6 +17,7 @@ class TasklistController < ApplicationController
   end
 
   def display
-    
+    @tasks = Tasklist.get_tasks
+    @task_count = Tasklist.get_task_count
   end
 end
--- app/models/tasklist.rb
+++ app/models/tasklist.rb
@@ -1,6 +1,23 @@
-class Tasklist < ActiveRecord :: Base
-  cattr_accessor :tasks
-  cattr_accessor :task_count
-  def addtasks name date
+class Tasklist
+  # cattr_accessor :tasks
+  # cattr_accessor :task_count
+  @tasks = []
+  @task_count = 0
+
+  def self.addtasks name, date
+    puts name
+    puts date
+    @tasks[@task_count] = [name, date]
+    @task_count += 1
+    puts @tasks
   end
+
+  def self.get_tasks
+    @tasks
+  end
+
+  def self.get_task_count
+    @task_count
+  end
+
 end
