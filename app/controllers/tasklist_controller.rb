c54c6fd469105649efb0e96c4019423b7c2cb55e
Hakumai <inside.rice164@gmail.com>
2018-07-25 13:01:31 +0900

    入力値が空であった場合にViewに警告を表示

--- app/controllers/tasklist_controller.rb
+++ app/controllers/tasklist_controller.rb
@@ -1,4 +1,7 @@
-class TasklistController < ApplicationController  
+class TasklistController < ApplicationController
+
+  @err_flag = false
+
   def insert
     if request.post? then
       name = params['name']
@@ -11,7 +14,11 @@ class TasklistController < ApplicationController
       elements = [name, year, month, day, hour, minute]
       if elements.all? {|t| !t.empty? && !t.nil?}
         Tasklist.addtasks(name, year.to_i, month.to_i, day.to_i, hour.to_i, minute.to_i)
+        @err_flag = false
         redirect_to :action =>"display"
+      else
+        @err_flag = true
+        render nothing: true, status: 400
       end
     end
   end
@@ -24,10 +31,12 @@ class TasklistController < ApplicationController
   def delete
     id = params['id']
     Tasklist.delete_task(id.to_i)
+    @err_flag = false
     redirect_to :action =>"display"
   end
 
   def display
+    @err_flag = false
     @tasks = Tasklist.get_tasks
     @task_count = Tasklist.get_task_count
   end
