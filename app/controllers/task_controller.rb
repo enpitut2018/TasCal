798af36506b675476c5bbcaa059d7befb5d261c0
ysumiya <y.sumiya@kde.cs.tsukuba.ac.jp>
2018-07-26 15:39:54 +0900

    時間計算をするメソッドを作成．

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -47,4 +47,35 @@ def delete
   end
   redirect_to :action =>"display"
 end
+
+def self.calc_available_time id
+    # 今の時間の取得
+    now = Time.zone.now
+    # deadlineの取得
+    taskdata = Task.find(id.to_i)
+    deadline = taskdata.deadline
+    available_time = deadline - now
+    
+    puts deadline
+    puts now
+    # 予定が入っている時間を引く
+    schedules = Schedule.all
+    schedules.each do |schedule| 
+      if schedule.start_time > now && schedule.end_time < deadline then
+        available_time -= (schedule.end_time - schedule.start_time)
+      elsif schedule.start_time > now && schedule.end_time > deadline then
+        available_time -= (deadline - schedule.start_time)
+      elsif schedule.start_time < now && schedule.end_time < deadline then
+        available_time -= (schedule.end_time - now)
+      elsif schedule.start_time < now && schedule.end_time > deadline then
+        available_time = 0
+      end
+    end
+
+    diff = ((deadline - now) * 24 * 60).to_i
+
+    # schedule_list = ScheduleController.display
+    (diff/60).to_s + "時間" + (diff%60).to_s + "分"
+  end
+
 end
