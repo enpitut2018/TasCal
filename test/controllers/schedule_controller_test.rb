d1f4d4cf8d2b40b24790139a516d66590b8d0794
ysumiya <y.sumiya@kde.cs.tsukuba.ac.jp>
2018-07-26 10:54:41 +0900

    deleteのテストを書いた．

--- test/controllers/schedule_controller_test.rb
+++ test/controllers/schedule_controller_test.rb
@@ -2,16 +2,16 @@ require 'test_helper'
 require 'date'
 
 class ScheduleControllerTest < ActionDispatch::IntegrationTest
-  # def setup
-  #   Schedule.destroy_all
-  # end
+  def setup
+    Schedule.destroy_all
+  end
 
   test "should get insert" do
     get schedule_insert_url
     assert_response :success
 
     assert_difference 'Schedule.where("id > 0").count' , 1 , "予定の数が増えたか" do
-      post params: 
+      post schedule_insert_url params: 
       { 
         name: "hoge", 
         s_year: "2018",
@@ -32,21 +32,45 @@ class ScheduleControllerTest < ActionDispatch::IntegrationTest
     # end
   end
 
-  test "should get display" do
-    get schedule_display_url
+  # test "should get display" do
+  #   get schedule_display_url
+  #   assert_response :success
+
+  #   assert_difference 'Schedule.where("id > 0").count' , -1 , "予定の数が減ったか" do
+  #     post params: 
+  #     { 
+  #       id: "1"
+  #     }
+  #   end
+  # end
+
+  test "should get delete" do
+    get schedule_delete_url
     assert_response :success
 
-    assert_difference 'Schedule.where("id > 0").count' , -1 , "予定の数が減ったか" do
-      post params: 
+    post schedule_insert_url params: 
       { 
-        id: "1"
+        name: "hoge", 
+        s_year: "2018",
+        s_month:"07",
+        s_day:"25",
+        s_hour:"12",
+        s_minute:"00",
+        e_year: "2018",
+        e_month:"07",
+        e_day:"29",
+        e_hour:"12",
+        e_minute:"00"
       }
-    end
-  end
+      p Schedule.find_by(name: "hoge")
+    assert_difference 'Schedule.find_by_name("hoge").count' , 1 , "予定が削除できたか" do
+      post schedule_delete_url params: 
+      {
+        id: id
+      }
 
-  # test "should get delete" do
-  #   get schedule_delete_url
-  #   assert_response :success
-  # end
+    end
+
+  end
 
 end
