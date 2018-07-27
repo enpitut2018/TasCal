dc14f87e4a1f03755bc351cf5a7dcc34aea06f6a
Hakumai <inside.rice164@gmail.com>
2018-07-27 16:01:22 +0900

    タスク一覧の表にBootstrapのデザインを適用

--- app/models/task.rb
+++ app/models/task.rb
@@ -14,9 +14,9 @@ class Task < ApplicationRecord
   def self.busy_color id
     rate = calc_rate_busy id
     if rate <= 0.6 then
-      "table-danger"
+      "bg-danger"
     elsif rate <= 0.8 then
-      "table-warning"
+      "bg-warning"
     else
       ""
     end
