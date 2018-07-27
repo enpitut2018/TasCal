b5117e88563bd9bc3ce82ec8a0a8d2e5b0de1dab
takaki <s1411385@coins.tsukuba.ac.jp>
2018-07-27 16:16:40 +0900

    エラーメッセージの詳細化

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,73 +1,75 @@
+# coding: utf-8
 require 'date'
 require 'time'
 class ScheduleController < ApplicationController
+  @err_id = "初期" #1:名前 2:日程 0:正常 -1:初期
 
-	@err_flag = false
+  def is_valid_date year, month, day, hour, minute
+    if Date.valid_date?(year,month,day) then
+      begin
+	parsed_time = Time.parse(hour.to_s + ":" + minute.to_s) 
+	return true
+      rescue ArgumentError => e
+	return false
+      end
+    else
+      return false
+    end
+  end
 
-	def is_valid_date year, month, day, hour, minute
-		if Date.valid_date?(year,month,day) then
-			begin
-				parsed_time = Time.parse(hour.to_s + ":" + minute.to_s) 
-				return true
-			rescue ArgumentError => e
-				return false
-			end
-		else
-			return false
-		end
-	end
+  def insert
+    if request.post? then
 
-	def insert
-		if request.post? then
+      name = params['name']
+      s_year = params['s_year']
+      s_month = params['s_month']
+      s_day = params['s_day']
+      s_hour = params['s_hour']
+      s_minute = params['s_minute']
 
-			name = params['name']
-			s_year = params['s_year']
-			s_month = params['s_month']
-			s_day = params['s_day']
-			s_hour = params['s_hour']
-			s_minute = params['s_minute']
+      e_year = params['e_year']
+      e_month = params['e_month']
+      e_day = params['e_day']
+      e_hour = params['e_hour']
+      e_minute = params['e_minute']
+      elements = [s_year, s_month, s_day, s_hour, s_minute,  e_year, e_month, e_day, e_hour, e_minute]
+      if (name.length <= 50 && name.length > 0) then 
+        if (elements.all? {|t| !t.empty? && !t.nil?}) &&
+	   self.is_valid_date(s_year.to_i, s_month.to_i, s_day.to_i, s_hour.to_i, s_minute.to_i) &&
+	   self.is_valid_date(e_year.to_i, e_month.to_i, e_day.to_i, e_hour.to_i, e_minute.to_i) then
+	  start_time = Time.zone.local(s_year.to_i, s_month.to_i, s_day.to_i, s_hour.to_i, s_minute.to_i)
+	  end_time = Time.zone.local(e_year.to_i, e_month.to_i, e_day.to_i, e_hour.to_i, e_minute.to_i)
+	  object = Schedule.new(
+          :name => name,
+          :start_time => start_time,
+          :end_time => end_time)
+	object.save
 
-			e_year = params['e_year']
-			e_month = params['e_month']
-			e_day = params['e_day']
-			e_hour = params['e_hour']
-			e_minute = params['e_minute']
+	@err_id = "正常"
+      #redirect_to :action =>"display"
+      else
+	@err_id = "日程"
+	render nothing: true, status: 400
+        end
+        @err_id = "名前"
+      end
+    end
+  end
 
-			elements = [name, s_year, s_month, s_day, s_hour, s_minute,  e_year, e_month, e_day, e_hour, e_minute]
-			if (elements.all? {|t| !t.empty? && !t.nil?}) &&
-				self.is_valid_date(s_year.to_i, s_month.to_i, s_day.to_i, s_hour.to_i, s_minute.to_i) &&
-				self.is_valid_date(e_year.to_i, e_month.to_i, e_day.to_i, e_hour.to_i, e_minute.to_i) then
-				start_time = Time.zone.local(s_year.to_i, s_month.to_i, s_day.to_i, s_hour.to_i, s_minute.to_i)
-				end_time = Time.zone.local(e_year.to_i, e_month.to_i, e_day.to_i, e_hour.to_i, e_minute.to_i)
-				object = Schedule.new(
-                                  :name => name,
-                                  :start_time => start_time,
-                                  :end_time => end_time)
-				object.save
-
-				@err_flag = false
-			        #redirect_to :action =>"display"
-			else
-				@err_flag = true
-				render nothing: true, status: 400
-			end
-		end
-	end
-
-	def display
-		@err_flag = false
+  def display
+    @err_id = "初期"
     @schedules = Schedule.all
-    p @schedules
-	end
+    # p @schedules
+  end
 
-	def delete
-		id = params['id']
+  def delete
+    id = params['id']
     object = Schedule.find(id)
     if !object.nil? then
-	    object.destroy
-	    @err_flag = false
-	  end
+      object.destroy
+      @err_id = "初期"
+    end
     #redirect_to :action =>"display"
     redirect_to :action =>"insert"
-	end
+  end
 end
