5f7fccfe460aac276746b72580de97d9afd7a013
Hakumai <inside.rice164@gmail.com>
2018-10-25 01:14:53 +0900

    SessionControllerのテストを整理

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,14 +1,31 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get login" do
-    get sessions_login_url
-    assert_response :success
+
+  test 'should not get login' do
+    # ログイン用のURLは "/login" に設定されており， "/sessions/login" は使用できない
+    assert_raises(ActionController::RoutingError) do
+      get '/sessions/login'
+    end
   end
 
-  test "should get logout" do
-    get sessions_logout_url
-    assert_response :success
+  test 'should not get logout' do
+    # ログアウト用のURLは "/logout" に設定されており， "/sessions/logout" は使用できない
+    assert_raises(ActionController::RoutingError) do
+      get '/sessions/logout'
+    end
+  end
+
+  test 'should be redirected to google authentication page' do
+    # ログイン用のURLにアクセスするとGoogleの認証ページにリダイレクトされる
+    get '/login'
+    assert_response :redirect
+  end
+
+  test 'should get logout' do
+    # ログアウト用のURLに（未ログイン状態で）アクセスするとトップページにリダイレクトされる
+    get '/logout'
+    assert_response :redirect
   end
 
 end
