b44f7ae1b32e8d7dc8682c97e69a5325d01c3373
Hakumai <inside.rice164@gmail.com>
2018-10-25 01:14:34 +0900

    emailカラムにデフォルト値を設定するとunique制約に引っかかる可能性があるため設定を変更

--- db/migrate/20181010145021_devise_create_users.rb
+++ db/migrate/20181010145021_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def self.up
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
 
       #added by Sumiya
       t.string :users, :name
