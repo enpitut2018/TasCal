063e3a2bb69335aadd3b6067b3e5fdfaa9bc8a90
takashift <e145771@ie.u-ryukyu.ac.jp>
2018-11-26 01:01:49 +0900

    task.rbにcalc_available_time()を作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -37,4 +37,34 @@ class Task < ApplicationRecord
   def self.calc_splited_time(id, now=Time.zone.now())
     now - Time.zone.now()
   end
+
+  def self.calc_available_time task_record_or_id, current_time=nil
+    taskdata = task_record_or_id.instance_of?(Task) ? task_record_or_id : Task.find(task_record_or_id.to_i)
+    deadline = taskdata.deadline
+
+    tasks = Task.all
+    tasks.sort{ |a, b| a.deadline <=> b.deadline }
+    n = 0
+    tmp_task = []
+    tasks.each do |task|
+      # available_time の計算に係るタスクの総数
+      if task.deadline > now && task.deadline < deadline then
+        n += n
+        tmp_task << task.id
+      end
+    end
+
+    available_time = 0
+    k = 1
+    tasks.each do |task|
+      # 一番現在時刻に近い締切のタスクの available_time を最初に計算して、それを2番目以降に締め切りの近いタスクの available_time の計算に利用する。
+      # もし、他のタスクの期限が現在時刻よりあとだったら（本当は自分の期限より前のタスクの計算も個別に行う？）
+      # 期限までのタスクの数だけデッドラインまでの空き時間を分割する
+      if task.deadline > now && task.deadline < deadline then
+        available_time += (TaskController.calc_available_time(tmp_task[k-1]) - available_time) / (n - k + 1)
+        k += 1
+      end
+    end
+  end
+  available_time
 end
