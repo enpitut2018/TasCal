9403e45bff5da96df3789e135f5688cff9b2bbf6
takaki <s1411385@coins.tsukuba.ac.jp>
2019-02-20 17:10:53 +0900

    expand devise scope

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
                   ENV['GOOGLE_CLIENT_ID'],
                   ENV['GOOGLE_CLIENT_SECRET'],
                   name: :google,
-                  scope: %w(email)
+                  scope: ["email", Google::Apis::CalendarV3::AUTH_CALENDAR]
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
